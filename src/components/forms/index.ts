66dd3551d6441346de7cd4a42eb20328f130fc7b
Christophe Mostefaoui <c.mostefaoui@yahoo.fr>
2025-12-01 14:40:52 +0100

    feat(SP-119): add FormDatePicker component with react-day-picker
    Étape 6/7 - Form System
    
    Composants créés:
    - FormDatePicker.tsx: DatePicker avec popover
    - index.ts: Barrel export de tous les forms
    
    FormDatePicker features:
    - Intégration react-day-picker (déjà installé)
    - Locale français (date-fns/locale/fr)
    - Format PPP français (ex: 15 janvier 2024)
    - min/max dates support
    - Disabled dates array
    - Popover dropdown avec click outside
    - forwardRef
    - Dark mode complet
    - Custom styles DayPicker
    - Accessibilité (role="dialog", aria-modal)
    
    Usage:
    <FormDatePicker
      label="Date de début"
      minDate={new Date()}
      value={date}
      onChange={(d) => setValue("date", d)}
    />
    
    Barrel export:
    import { FormInput, FormSelect, ... } from '@/components/forms'

---
+++ src/components/forms/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Barrel export pour tous les composants de formulaire
+ * Import unique : import { FormInput, FormSelect, ... } from '@/components/forms'
+ */
+
+export * from "./FormField";
+export * from "./FormInput";
+export * from "./FormTextarea";
+export * from "./FormSelect";
+export * from "./FormCheckbox";
+export * from "./FormRadioGroup";
+export * from "./FormDatePicker";
