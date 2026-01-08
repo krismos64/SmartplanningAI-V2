3dd32367deb7b29a27e16e65d0fd23cc32e3d19a
Christophe Mostefaoui <c.mostefaoui@yahoo.fr>
2026-01-08 10:13:28 +0100

    SP-151: add CompanyFilters and DeleteCompanyDialog components

---
+++ src/components/admin/companies/index.ts
@@ -0,0 +1,10 @@
+/**
+ * Export des composants CRUD Companies
+ *
+ * @ticket SP-151
+ */
+
+export { CompanyForm } from './CompanyForm'
+export { CompanyFilters } from './CompanyFilters'
+export { DeleteCompanyDialog } from './DeleteCompanyDialog'
+export { createCompanyColumns, type CompanyActionsProps } from './columns'
